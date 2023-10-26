9ac792bebdb0614f9b34ee2ef021d04f81048b00
Aviansh Udata <60933551+avinashudata52@users.noreply.github.com>
2023-10-27 00:28:12 +0530

    Update stock_mgnt.sql

--- db/stock_mgnt.sql
+++ db/stock_mgnt.sql
@@ -2,7 +2,7 @@
 SQLyog Community v12.2.5 (32 bit)
 MySQL - 5.6.34 : Database - stock_mgnt
 *********************************************************************
-*/
+*/
 
 /*!40101 SET NAMES utf8 */;
 
@@ -15,6 +15,7 @@ MySQL - 5.6.34 : Database - stock_mgnt
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`stock_mgnt` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE `stock_mgnt`;
+
 
 /*Table structure for table `item_master` */
 
@@ -33,8 +34,10 @@ CREATE TABLE `item_master` (
 
 /*Data for the table `item_master` */
 
-insert  into `item_master`(`id`,`dt`,`itemnm`,`hsn`,`image`,`sts`,`edt`) values 
-(1,'2020-11-03','MONITOR ZEBION SPLAY 16(15.6)AV1','8528','0',0,'2020-11-03 09:53:23'),
+insert  into `item_master`(`id`,`dt`,`itemnm`,`hsn`,`image`,`sts`,`edt`) values 
+
+(1,'2020-11-03','MONITOR ZEBION SPLAY 16(15.6)AV1','8528','0',0,'2020-11-03 09:53:23'),
+
 (2,'2020-11-03','WALL MOUNT RACK 2U/400MM(WIDTH)','84733099','0',0,'2020-11-03 09:53:36');
 
 /*Table structure for table `login_tbl` */
@@ -54,7 +57,8 @@ CREATE TABLE `login_tbl` (
 
 /*Data for the table `login_tbl` */
 
-insert  into `login_tbl`(`id`,`uname`,`mobile`,`emailId`,`pwd`,`sts`,`edt`) values 
+insert  into `login_tbl`(`id`,`uname`,`mobile`,`emailId`,`pwd`,`sts`,`edt`) values 
+
 (1,'admin','0','admin@gmail.com','0101',0,'2020-11-03 10:24:14');
 
 /*Table structure for table `party_details` */
@@ -77,8 +81,10 @@ CREATE TABLE `party_details` (
 
 /*Data for the table `party_details` */
 
-insert  into `party_details`(`id`,`company_nm`,`mobile`,`adrs`,`email`,`gst_no`,`state`,`city_pin`,`sts`,`edt`) values 
-(1,'UNIQUE SOLUTIONS','9890117602','SOLAPUR','ravikirankota92@gmail.com','123456','1','413005',0,'2020-11-24 22:19:47'),
+insert  into `party_details`(`id`,`company_nm`,`mobile`,`adrs`,`email`,`gst_no`,`state`,`city_pin`,`sts`,`edt`) values 
+
+(1,'UNIQUE SOLUTIONS','9890117602','SOLAPUR','ravikirankota92@gmail.com','123456','1','413005',0,'2020-11-24 22:19:47'),
+
 (2,'VENKANT INFOTECH','8668749439','SOLAPUR','kotakiran028@gmail.com','6548465','2','413006',0,'2020-11-24 22:20:57');
 
 /*Table structure for table `puchase_entry_info` */
@@ -99,10 +105,14 @@ CREATE TABLE `puchase_entry_info` (
 
 /*Data for the table `puchase_entry_info` */
 
-insert  into `puchase_entry_info`(`id`,`lastId`,`hsc_code`,`item_nm`,`rate`,`qty`,`total`,`edt`) values 
-(1,'1','8528','1','2500','5','12500','2020-11-24 22:37:44'),
-(2,'1','84733099','2','1250','2','2500','2020-11-24 22:37:44'),
-(3,'2','8528','1','2500','5','12500','2020-11-24 22:40:35'),
+insert  into `puchase_entry_info`(`id`,`lastId`,`hsc_code`,`item_nm`,`rate`,`qty`,`total`,`edt`) values 
+
+(1,'1','8528','1','2500','5','12500','2020-11-24 22:37:44'),
+
+(2,'1','84733099','2','1250','2','2500','2020-11-24 22:37:44'),
+
+(3,'2','8528','1','2500','5','12500','2020-11-24 22:40:35'),
+
 (4,'2','84733099','2','1250','2','2500','2020-11-24 22:40:35');
 
 /*Table structure for table `purchase_entry` */
@@ -135,8 +145,10 @@ CREATE TABLE `purchase_entry` (
 
 /*Data for the table `purchase_entry` */
 
-insert  into `purchase_entry`(`id`,`party_id`,`bill_no`,`date`,`purchase_type`,`gst_type`,`total`,`discount_type`,`discnt_amt_per`,`dis_total`,`sgst`,`cgst`,`igst`,`grand_tot`,`pay_bill_amt`,`payamt`,`balamt`,`stock_sts`,`sts`,`edt`) values 
-(1,'1','112233','2020-11-24',1,2,'15000',1,'15.2542','2288.13','762.71','762.71','0','14237',1,'12000.15','2237',1,0,'2020-11-24 22:37:44'),
+insert  into `purchase_entry`(`id`,`party_id`,`bill_no`,`date`,`purchase_type`,`gst_type`,`total`,`discount_type`,`discnt_amt_per`,`dis_total`,`sgst`,`cgst`,`igst`,`grand_tot`,`pay_bill_amt`,`payamt`,`balamt`,`stock_sts`,`sts`,`edt`) values 
+
+(1,'1','112233','2020-11-24',1,2,'15000',1,'15.2542','2288.13','762.71','762.71','0','14237',1,'12000.15','2237',1,0,'2020-11-24 22:37:44'),
+
 (2,'2','2233156','2020-11-24',1,1,'15000',1,'15.2542','2288.13','0','0','2288.14','15000',1,'3000','12000',1,0,'2020-11-24 22:40:34');
 
 /*Table structure for table `purchase_outstanding` */
@@ -158,11 +170,16 @@ CREATE TABLE `purchase_outstanding` (
 
 /*Data for the table `purchase_outstanding` */
 
-insert  into `purchase_outstanding`(`id`,`purchase_id`,`receipt_no`,`bill_no`,`date`,`grand_tot`,`paid_amt`,`bal_amt`,`edt`) values 
-(1,1,'1','112233','2020-11-24','14237','12000.15','2237','2020-11-24 22:37:44'),
-(2,1,'2','112233','2020-11-24','14237','2236','0.849999999999909','2020-11-24 22:38:26'),
-(3,2,'3','2233156','2020-11-24','15000','3000','12000','2020-11-24 22:40:35'),
-(4,2,'4','2233156','2020-11-24','15000','1000','11000','2020-11-24 23:59:36'),
+insert  into `purchase_outstanding`(`id`,`purchase_id`,`receipt_no`,`bill_no`,`date`,`grand_tot`,`paid_amt`,`bal_amt`,`edt`) values 
+
+(1,1,'1','112233','2020-11-24','14237','12000.15','2237','2020-11-24 22:37:44'),
+
+(2,1,'2','112233','2020-11-24','14237','2236','0.849999999999909','2020-11-24 22:38:26'),
+
+(3,2,'3','2233156','2020-11-24','15000','3000','12000','2020-11-24 22:40:35'),
+
+(4,2,'4','2233156','2020-11-24','15000','1000','11000','2020-11-24 23:59:36'),
+
 (5,1,'5','112233','2020-11-24','14237','0.85','3.6004532688593827e-13','2020-11-24 23:59:52');
 
 /*Table structure for table `purchase_party_details` */
@@ -185,8 +202,10 @@ CREATE TABLE `purchase_party_details` (
 
 /*Data for the table `purchase_party_details` */
 
-insert  into `purchase_party_details`(`id`,`company_nm`,`mobile`,`adrs`,`email`,`gst_no`,`state`,`city_pin`,`sts`,`edt`) values 
-(1,'SHRIAPP','9561294216','SOLAPUR','ravikirankota29@gmail.com','651782154',1,'413009',0,'2020-11-24 22:22:11'),
+insert  into `purchase_party_details`(`id`,`company_nm`,`mobile`,`adrs`,`email`,`gst_no`,`state`,`city_pin`,`sts`,`edt`) values 
+
+(1,'SHRIAPP','9561294216','SOLAPUR','ravikirankota29@gmail.com','651782154',1,'413009',0,'2020-11-24 22:22:11'),
+
 (2,'DREAM TECHNOLOGY','9890117602','SOLAPUR','uniquesolutions@gmail.com','65489687',2,'413007',0,'2020-11-24 22:23:04');
 
 /*Table structure for table `sale_billno` */
@@ -202,10 +221,14 @@ CREATE TABLE `sale_billno` (
 
 /*Data for the table `sale_billno` */
 
-insert  into `sale_billno`(`id`,`bill_no`,`edt`) values 
-(1,'US00001','2020-11-24 23:00:43'),
-(2,'US00002','2020-11-24 23:27:49'),
-(3,'US00003','2020-11-24 23:52:23'),
+insert  into `sale_billno`(`id`,`bill_no`,`edt`) values 
+
+(1,'US00001','2020-11-24 23:00:43'),
+
+(2,'US00002','2020-11-24 23:27:49'),
+
+(3,'US00003','2020-11-24 23:52:23'),
+
 (4,'US00004','2020-11-24 23:56:16');
 
 /*Table structure for table `sale_entry` */
@@ -237,10 +260,14 @@ CREATE TABLE `sale_entry` (
 
 /*Data for the table `sale_entry` */
 
-insert  into `sale_entry`(`id`,`party_id`,`bill_no`,`date`,`sale_type`,`gst_type`,`total`,`discount_type`,`discnt_amt_per`,`dis_total`,`sgst`,`cgst`,`igst`,`grand_tot`,`pay_bill_amt`,`payamt`,`balamt`,`sts`,`edt`) values 
-(1,'1','US00001','2020-11-24',1,2,'11250',1,'15.2542','1716.0975','572.03','572.03','0','9534',2,'1200','9534',0,'2020-11-24 23:00:43'),
-(2,'2','US00002','2020-11-24',1,1,'11250',1,'13.75','1546.875','0','0','1746.56','11450',1,'4500','6950',0,'2020-11-24 23:27:50'),
-(3,'1','US00003','2020-11-24',2,4,'5000',1,'15.53','776.5','0','0','0','4224',2,'0','4224',0,'2020-11-24 23:52:23'),
+insert  into `sale_entry`(`id`,`party_id`,`bill_no`,`date`,`sale_type`,`gst_type`,`total`,`discount_type`,`discnt_amt_per`,`dis_total`,`sgst`,`cgst`,`igst`,`grand_tot`,`pay_bill_amt`,`payamt`,`balamt`,`sts`,`edt`) values 
+
+(1,'1','US00001','2020-11-24',1,2,'11250',1,'15.2542','1716.0975','572.03','572.03','0','9534',2,'1200','9534',0,'2020-11-24 23:00:43'),
+
+(2,'2','US00002','2020-11-24',1,1,'11250',1,'13.75','1546.875','0','0','1746.56','11450',1,'4500','6950',0,'2020-11-24 23:27:50'),
+
+(3,'1','US00003','2020-11-24',2,4,'5000',1,'15.53','776.5','0','0','0','4224',2,'0','4224',0,'2020-11-24 23:52:23'),
+
 (4,'1','US00004','2020-11-24',1,3,'2500',0,'100','100','60.00','60.00','0','2520',1,'520','2000',0,'2020-11-24 23:56:17');
 
 /*Table structure for table `sale_entry_info` */
@@ -262,12 +289,18 @@ CREATE TABLE `sale_entry_info` (
 
 /*Data for the table `sale_entry_info` */
 
-insert  into `sale_entry_info`(`id`,`lastId`,`hsc_code`,`item_nm`,`stock_qty`,`qty`,`rate`,`total`,`edt`) values 
-(1,'1','8528','1','10','3','2500','7500','2020-11-24 23:00:43'),
-(2,'1','84733099','2','4','3','1250','3750','2020-11-24 23:00:43'),
-(3,'2','8528','1','7','4','2500','10000','2020-11-24 23:27:50'),
-(4,'2','84733099','2','1','1','1250','1250','2020-11-24 23:27:50'),
-(5,'3','8528','1','3','2','2500','5000','2020-11-24 23:52:23'),
+insert  into `sale_entry_info`(`id`,`lastId`,`hsc_code`,`item_nm`,`stock_qty`,`qty`,`rate`,`total`,`edt`) values 
+
+(1,'1','8528','1','10','3','2500','7500','2020-11-24 23:00:43'),
+
+(2,'1','84733099','2','4','3','1250','3750','2020-11-24 23:00:43'),
+
+(3,'2','8528','1','7','4','2500','10000','2020-11-24 23:27:50'),
+
+(4,'2','84733099','2','1','1','1250','1250','2020-11-24 23:27:50'),
+
+(5,'3','8528','1','3','2','2500','5000','2020-11-24 23:52:23'),
+
 (6,'4','8528','1','1','1','2500','2500','2020-11-24 23:56:17');
 
 /*Table structure for table `sale_outstanding` */
@@ -289,15 +322,24 @@ CREATE TABLE `sale_outstanding` (
 
 /*Data for the table `sale_outstanding` */
 
-insert  into `sale_outstanding`(`id`,`sale_id`,`receipt_no`,`bill_no`,`date`,`grand_tot`,`paid_amt`,`bal_amt`,`edt`) values 
-(1,1,'1','US00001','2020-11-24','9534','1200','9534','2020-11-24 23:00:43'),
-(2,2,'2','US00002','2020-11-24','11450','4500','6950','2020-11-24 23:27:50'),
-(3,3,'3','US00003','2020-11-24','4224','0','4224','2020-11-24 23:52:23'),
-(4,3,'4','US00003','2020-11-24','4224','224','4000','2020-11-24 23:54:02'),
-(5,3,'4','US00003','2020-11-24','4224','2500','1500','2020-11-24 23:54:37'),
-(6,1,'4','US00001','2020-11-24','9534','5689','2645','2020-11-24 23:54:55'),
-(7,1,'4','US00001','2020-11-24','9534','2645','0','2020-11-24 23:55:07'),
-(8,4,'8','US00004','2020-11-24','2520','520','2000','2020-11-24 23:56:17'),
+insert  into `sale_outstanding`(`id`,`sale_id`,`receipt_no`,`bill_no`,`date`,`grand_tot`,`paid_amt`,`bal_amt`,`edt`) values 
+
+(1,1,'1','US00001','2020-11-24','9534','1200','9534','2020-11-24 23:00:43'),
+
+(2,2,'2','US00002','2020-11-24','11450','4500','6950','2020-11-24 23:27:50'),
+
+(3,3,'3','US00003','2020-11-24','4224','0','4224','2020-11-24 23:52:23'),
+
+(4,3,'4','US00003','2020-11-24','4224','224','4000','2020-11-24 23:54:02'),
+
+(5,3,'4','US00003','2020-11-24','4224','2500','1500','2020-11-24 23:54:37'),
+
+(6,1,'4','US00001','2020-11-24','9534','5689','2645','2020-11-24 23:54:55'),
+
+(7,1,'4','US00001','2020-11-24','9534','2645','0','2020-11-24 23:55:07'),
+
+(8,4,'8','US00004','2020-11-24','2520','520','2000','2020-11-24 23:56:17'),
+
 (9,4,'4','US00004','2020-11-24','2520','1000','1000','2020-11-24 23:56:55');
 
 /*Table structure for table `stock_info` */
@@ -319,10 +361,14 @@ CREATE TABLE `stock_info` (
 
 /*Data for the table `stock_info` */
 
-insert  into `stock_info`(`id`,`lastId`,`item`,`qty`,`p_rate`,`p_total`,`s_rate`,`s_total`,`edt`) values 
-(1,'1','1','5','2500','12500','0','0','2020-11-24 22:56:28'),
-(2,'1','2','2','1250','2500','0','0','2020-11-24 22:56:29'),
-(3,'2','1','5','2500','12500','0','0','2020-11-24 22:56:35'),
+insert  into `stock_info`(`id`,`lastId`,`item`,`qty`,`p_rate`,`p_total`,`s_rate`,`s_total`,`edt`) values 
+
+(1,'1','1','5','2500','12500','0','0','2020-11-24 22:56:28'),
+
+(2,'1','2','2','1250','2500','0','0','2020-11-24 22:56:29'),
+
+(3,'2','1','5','2500','12500','0','0','2020-11-24 22:56:35'),
+
 (4,'2','2','2','1250','2500','0','0','2020-11-24 22:56:35');
 
 /*Table structure for table `stock_maintain` */
@@ -343,8 +389,10 @@ CREATE TABLE `stock_maintain` (
 
 /*Data for the table `stock_maintain` */
 
-insert  into `stock_maintain`(`id`,`dt`,`itemId`,`prev_rate`,`rate`,`qty`,`bal`,`edt`) values 
-(1,'2020-11-24','1','0','2500','0','10','2020-11-24 22:56:29'),
+insert  into `stock_maintain`(`id`,`dt`,`itemId`,`prev_rate`,`rate`,`qty`,`bal`,`edt`) values 
+
+(1,'2020-11-24','1','0','2500','0','10','2020-11-24 22:56:29'),
+
 (2,'2020-11-24','2','0','1250','0','4','2020-11-24 22:56:29');
 
 /*Table structure for table `stock_master` */
@@ -361,8 +409,10 @@ CREATE TABLE `stock_master` (
 
 /*Data for the table `stock_master` */
 
-insert  into `stock_master`(`id`,`dt`,`purchase_invoice`,`edt`) values 
-(1,'2020-11-24','1','2020-11-24 22:56:28'),
+insert  into `stock_master`(`id`,`dt`,`purchase_invoice`,`edt`) values 
+
+(1,'2020-11-24','1','2020-11-24 22:56:28'),
+
 (2,'2020-11-24','2','2020-11-24 22:56:35');
 
 /* Procedure structure for procedure `ab` */
